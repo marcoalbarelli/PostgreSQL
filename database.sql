07d0b9e03b5a33d3f7941bc3794255a046f0c24c
Andrea Adami <fol@fulcro.net>
2013-08-12 16:07:32 +0200

    creazione iniziale

---
+++ database.sql
@@ -0,0 +1,11 @@
+-- Database: scuola247
+
+-- DROP DATABASE scuola247;
+
+CREATE DATABASE scuola247
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Italian_Italy.1252'
+       LC_CTYPE = 'Italian_Italy.1252'
+       CONNECTION LIMIT = -1;
